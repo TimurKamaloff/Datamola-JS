543a0ac03433770dabababa79b74106a1eabd09a
Timur Kamalov <kamalow.t@yandex.by>
2020-10-30 00:35:27 +0300

    extra

---
+++ Extra tasks/3.2/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let broker = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length-1; i++) {
+        let buy = arr[i];
+        let sale = arr[i+1];
+        if (sale > buy) sum += (sale - buy);
+    }
+    console.log(sum);
+    return sum;
+}
+
+broker([7,1,5,3,6,4]);
+broker([1,2,3,4,5]);
+broker([7,6,4,3,1]);
+broker([7,1,5,3,16,4]);
+broker([]);
