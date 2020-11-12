77447b49b0452d57b5758229c61a50ebf10a23e8
Timur Kamalov <kamalow.t@yandex.bt>
2020-11-12 18:19:09 +0300

    extra

---
+++ Extra tasks/3.2/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let broker = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length-1; i++) {
+        let buy = arr[i];
+        let sale = arr[i+1];
+        if (sale > buy) sum += (sale - buy);
+    }
+    console.log(sum);
+    return sum;
+}
+
+broker([7,1,5,3,6,4]);
+broker([1,2,3,4,5]);
+broker([7,6,4,3,1]);
+broker([7,1,5,3,16,4]);
+broker([0,1,5,3,7,10,2]);
+broker([]); // на всякий случай проверил
